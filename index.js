bcc60e3fa97324f3c09dbf5fb8046fa9d5d36b79
SunChenW <781057070@qq.com>
2019-08-10 08:53:01 +0800

    1

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 $(function() {
-
+				//将iframe的内容转移到index.html
 				function iframeTextChang(){
 					$(".container_iframe").empty().css({
 						"z-index":1
 					});
+					$(".dropdown").css({
+						"z-index":1
+					});
+					$(".aside_doc").find().not(".dropdown-toggle").remove()
+					$("#mf").contents().find("body").find("ul").eq(0).addClass("dropdown-menu").appendTo(".aside_doc")
 					$("#mf").contents().find("body").children().appendTo(".container_iframe")
 					$("#mf").contents().find("head link[rel='stylesheet']").appendTo("head")
 				}
-				$("a[href='javascript:;']").click(function() {
-					$("iframe").attr("src", $(this).attr("data_href"))
-				}) 
 				
 				$("iframe").load(function(){
 					iframeTextChang()
 				})
-				
+				//点击导航切换内容
+				$(".navbar a[href='javascript:;']").click(function() {
+					$("iframe").attr("src", $(this).attr("data_href"))
+				}) 
+				//试图阻止移动端触摸默认行为
 				$(document).on("touchmove", function(e) {
 					e.preventDefault() 
 					e.stopPropagation() 
 					return false
 				})
+				//回到顶部
 				$(".gotop").click(function(){
 							$("body,html").stop().animate({
 								scrollTop:10
